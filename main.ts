9ac239ec0d6cd60d4401599b22e312904e031b19
Coecu <102954997+Coecu@users.noreply.github.com>
2022-04-04 11:10:40 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,34 +1,50 @@
-basic.forever(function () {
+let Go = 0
+input.onButtonPressed(Button.A, function () {
+    Go = 1
     basic.showLeds(`
-        . # . # #
-        . . # . .
         . # . # .
-        # . # . .
-        # # . . .
+        . # . # .
+        . . # . .
+        # . . . #
+        . # # # .
         `)
-    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 255)
+})
+input.onButtonPressed(Button.B, function () {
+    Go = 0
+    basic.showLeds(`
+        . . . . .
+        # # . # #
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+})
+basic.forever(function () {
+    if (Go == 1) {
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+            maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 255)
 maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 255)
-    } else {
-        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-            maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 0)
-maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 255)
-if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+        } else {
+            if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
                 maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 0)
 maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 255)
-            }
-        } else {
-            if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-                maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 255)
-maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 0)
 if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-                    maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 255)
-maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 0)
+                    maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 0)
+maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 255)
                 }
+            } else {
                 if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
                     maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 255)
-                } else {
-                    maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 0)
+maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 0)
+if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+                        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 255)
+maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 0)
+                    }
+                    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+                        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 255)
+                    } else {
+                        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 0)
+                    }
                 }
             }
         }
